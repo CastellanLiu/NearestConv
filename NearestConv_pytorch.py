d6474b1a3b76e18ac4f10249ccf73c186624a492
Ming Liu <lm.945@foxmail.com>
2021-06-08 12:58:09 +0800

    Update NearestConv_pytorch.py

--- NearestConv_pytorch.py
+++ NearestConv_pytorch.py
@@ -7,7 +7,7 @@ class NearestConv(torch.nn.ConvTranspose2d):
         assert conv.stride == (1, 1)
         assert all(i*2+1==j for i,j in zip(conv.padding, conv.kernel_size))
         
-        kernel_size = (i+1 for i in conv.kernel_size)
+        kernel_size = tuple(i+1 for i in conv.kernel_size)
         
         super(NearestConv, self).__init__(conv.in_channels,
                                           conv.out_channels,
@@ -15,6 +15,7 @@ class NearestConv(torch.nn.ConvTranspose2d):
                                           stride=2,
                                           padding=conv.padding,
                                           bias=conv.bias is not None)
+        self.to(conv.weight.data.device)
         
         raw_weight = conv.weight.data.transpose(0, 1).flip((2,3))
         self.weight.data.zero_()
