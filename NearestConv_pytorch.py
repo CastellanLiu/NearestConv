0ad0b210bbea6007b18f821bd054101a4a4fc2d3
Ming Liu <lm.945@foxmail.com>
2021-06-08 10:57:31 +0800

    Adapt to arbitrary Nearest + (odd x odd) Conv

--- NearestConv_pytorch.py
+++ NearestConv_pytorch.py
@@ -1,21 +1,22 @@
 import torch
 
 class NearestConv(torch.nn.ConvTranspose2d):
-    def __init__(self, conv3x3):
+    def __init__(self, conv):
         
-        assert conv3x3.kernel_size == (3, 3)
-        assert conv3x3.stride == (1, 1)
-        assert conv3x3.padding == (1, 1)
+        assert all(i % 2 == 1 for i in conv.kernel_size)
+        assert conv.stride == (1, 1)
+        assert all(i*2+1==j for i,j in zip(conv.padding, conv.kernel_size))
         
-        super(NearestConv, self).__init__(conv3x3.in_channels,
-                                          conv3x3.out_channels,
-                                          kernel_size=4,
+        kernel_size = (i+1 for i in conv.kernel_size)
+        
+        super(NearestConv, self).__init__(conv.in_channels,
+                                          conv.out_channels,
+                                          kernel_size=kernel_size,
                                           stride=2,
-                                          padding=1,
-                                          bias=conv3x3.bias is not None)
+                                          padding=conv.padding,
+                                          bias=conv.bias is not None)
         
-        raw_weight = conv3x3.weight.data.transpose(0, 1).flip((2,3))
-
+        raw_weight = conv.weight.data.transpose(0, 1).flip((2,3))
         self.weight.data.zero_()
         self.weight.data[..., 1:,1:] += raw_weight
         self.weight.data[..., 1:,:-1] += raw_weight
@@ -23,4 +24,4 @@ class NearestConv(torch.nn.ConvTranspose2d):
         self.weight.data[..., :-1,:-1] += raw_weight
         
         if self.bias is not None:
-            self.bias.data.copy_(conv3x3.bias)
+            self.bias.data.copy_(conv.bias)
