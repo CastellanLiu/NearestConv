843c09c48415ab0829ffa32fcb12c5ad252a046c
Ming Liu <lm.945@foxmail.com>
2021-06-08 10:46:30 +0800

    Repair bugs for multi-channel
    In PyTorch:
    Conv weight: [out_channels, in_channels, kH, kW]
    TConv weight: [in_channels, out_channels, kH, kW]

--- NearestConv_pytorch.py
+++ NearestConv_pytorch.py
@@ -2,20 +2,25 @@ import torch
 
 class NearestConv(torch.nn.ConvTranspose2d):
     def __init__(self, conv3x3):
+        
         assert conv3x3.kernel_size == (3, 3)
         assert conv3x3.stride == (1, 1)
         assert conv3x3.padding == (1, 1)
+        
         super(NearestConv, self).__init__(conv3x3.in_channels,
-                                     conv3x3.out_channels,
-                                     kernel_size=4,
-                                     stride=2,
-                                     padding=1,
-                                     bias=conv3x3.bias is not None)
+                                          conv3x3.out_channels,
+                                          kernel_size=4,
+                                          stride=2,
+                                          padding=1,
+                                          bias=conv3x3.bias is not None)
+        
+        raw_weight = conv3x3.weight.data.transpose(0, 1).flip((2,3))
+
         self.weight.data.zero_()
-        self.weight.data[..., 1:,1:] += conv3x3.weight.data
-        self.weight.data[..., 1:,:-1] += conv3x3.weight.data
-        self.weight.data[..., :-1,1:] += conv3x3.weight.data
-        self.weight.data[..., :-1,:-1] += conv3x3.weight.data
-        self.weight.data = torch.flip(self.weight.data, (2,3))
+        self.weight.data[..., 1:,1:] += raw_weight
+        self.weight.data[..., 1:,:-1] += raw_weight
+        self.weight.data[..., :-1,1:] += raw_weight
+        self.weight.data[..., :-1,:-1] += raw_weight
+        
         if self.bias is not None:
             self.bias.data.copy_(conv3x3.bias)
